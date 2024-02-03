5643cdf24b7e7895b7bdfe044b3e4aea2a2e5b03
glckfndr <obulan65@gmail.com>
2024-02-03 07:42:20 +0200

    db(image): seeds db with images

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+7.times do
+  Image.create title: FFaker::CheesyLingo.word, url: FFaker::Image.url
+end
