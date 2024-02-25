42c6ddb9ac91e70ec4c049e2f31fca2d65c51b6b
glckfndr <obulan65@gmail.com>
2024-02-26 00:27:36 +0200

    feat: Add images-target

--- app/javascript/controllers/images_controller.js
+++ app/javascript/controllers/images_controller.js
@@ -1,10 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
+  static targets = ['image'];
+  connect(){
+    const title = document.createElement('p');
+    title.textContent = this.imageTarget.alt;
+    this.element.appendChild(title);
+  }
 
   getUrl(e){
     this.dispatch("copy", {detail: {content: `Image ${e.target.src} has been copied!`}})
     navigator.clipboard.writeText(e.target.src)
 
+  }
+
+  imageTargetConnected(e){
+    console.log(e.alt)
   }
 }
