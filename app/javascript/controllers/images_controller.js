4af3b5179e8635974db81d7d1c03ed245e0ce742
glckfndr <obulan65@gmail.com>
2024-02-27 15:57:09 +0200

    feat: add saveTitle for ImagesController

--- app/javascript/controllers/images_controller.js
+++ app/javascript/controllers/images_controller.js
@@ -1,37 +1,62 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ['image', 'title', 'save'];
-  connect(){
-    const title = document.createElement('p');
+  static targets = ["image", "title", "save"];
+  static classes = ["loading"];
+  static values = { id: String };
+  connect() {
+    const title = document.createElement("p");
     title.textContent = this.imageTarget.alt;
     title.contentEditable = true;
-    title.dataset.imagesTarget = 'title'
-    title.dataset.action = 'click->images#editTitle'
+    title.dataset.imagesTarget = "title";
+    title.dataset.action = "click->images#editTitle";
     this.element.appendChild(title);
   }
 
-  editTitle(e){
-    if(!this.hasSaveTarget){
-    const btn = document.createElement('button')
-    btn.textContent = 'Save'
-    btn.dataset.imagesTarget = 'save'
-    btn.classList = 'btn btn-primary btn-sm'
-    e.target.insertAdjacentElement('afterend', btn)
+  editTitle(e) {
+    if (!this.hasSaveTarget) {
+      const btn = document.createElement("button");
+      btn.textContent = "Save";
+      btn.dataset.imagesTarget = "save";
+      btn.dataset.action = "click->images#saveTitle";
+      btn.classList = "btn btn-primary btn-sm";
+      e.target.insertAdjacentElement("afterend", btn);
     }
   }
 
-  getUrl(e){
-    this.dispatch("copy", {detail: {content: `Image ${e.target.src} has been copied!`}})
-    navigator.clipboard.writeText(e.target.src)
+  async saveTitle(ev) {
+    ev.preventDefault();
+    ev.target.disabled = true;
+    ev.target.classList.add(this.loadingClass);
 
+    const formData = new FormData();
+    formData.append("image[title]", this.titleTarget.innerText);
+    await this.doPatch(`/api/images/${this.idValue}`, formData);
+    ev.target.remove();
   }
 
-  imageTargetConnected(e){
-    console.log(e.alt)
+  getUrl(e) {
+    this.dispatch("copy", {
+      detail: { content: `Image ${e.target.src} has been copied!` },
+    });
+    navigator.clipboard.writeText(e.target.src);
   }
 
-  titleTargetConnected(el){
+  imageTargetConnected(e) {
+    console.log(e.alt);
+  }
+
+  titleTargetConnected(el) {
     console.log(el);
+  }
+
+  async doPatch(url, body) {
+    const csrfToken = document.getElementsByName("csrf-token")[0].content;
+
+    await fetch(url, {
+      method: "PATCH",
+      body: body,
+      headers: { "X-CSRF-Token": csrfToken },
+    });
   }
 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :images, only: :update
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   root 'images#index'
 end
